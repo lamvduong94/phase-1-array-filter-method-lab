22534d41275d1441ceb2ee998212015039072d13
Lam <LamDuong.inbox@gmail.com>
2021-09-09 00:05:15 -0700

    commit tree 60c134735b08e5706ed029af8b0fc2f6ff408e46

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().slice(0, 2) === letters.toLowerCase();
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    })
+}
